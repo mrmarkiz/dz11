3a16d2f0804a0a1498def5287604d54cffb7747b
mrmarkiz <maxtomusiak315@gmail.com>
2023-04-29 00:06:18 +0300

    Add project files.

---
+++ dz11/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace dz11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+            do
+            {
+                Console.Write("Enter task to run: ");
+                int.TryParse(Console.ReadLine(), out choice);
+                switch (choice)
+                {
+                    case 1:
+                        Task1.Run();
+                        break;
+
+                }
+            } while (choice != 0);
+        }
+    }
+}
---
+++ dz11/Task1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz11
+{
+    public static class intExtensions
+    {
+        public static bool IsFibonacci(this int number)
+        {
+            int a = 0, b = 1, c = 1, tmp;
+            while (a < number)
+            {
+                tmp = b + c;
+                a = b;
+                b = c;
+                c = tmp;
+            }
+            return number == a;
+        }
+    }
+
+    internal class Task1
+    {
+        public static void Run()
+        {
+            Console.Write("Enter number to check on being Fibonacci's: ");
+            if (int.TryParse(Console.ReadLine(), out int number))
+                Console.WriteLine($"Result: {number.IsFibonacci()}");
+            else
+                Console.WriteLine("Wrong number!");
+        }
+    }
+}
